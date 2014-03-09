ce8ab9d36dece30b75083cd444ecba7591a3e53b
Lei Xu <eddyxu@gmail.com>
2014-03-09 14:31:28 -0500

    * 0.1.5 release

--- coveralls/__init__.py
+++ coveralls/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 __author__ = 'Lei Xu <eddyxu@gmail.com>'
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __classifiers__ = [
     'Development Status :: 2 - Pre-Alpha',
