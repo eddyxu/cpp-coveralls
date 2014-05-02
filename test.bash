4a54db0c134887403975478f70073a1ac31903ba
Lei Xu <eddyxu@gmail.com>
2014-05-02 06:58:31 -0700

    Support trying multiple encodings
    Note that "--encodings" is compatible with "--encoding" since argparse
    auto-completes the latter to the former.
    
    This fixes #26.

--- test.bash
+++ test.bash
@@ -2,8 +2,10 @@
 #
 # Run system test.
 
+coveralls --help
+
 echo 'int main() {return 0;}' > foo.c
 gcc -coverage -o foo foo.c
 ./foo
-coveralls --verbose | grep 'coverage' | grep '1'
+coveralls --verbose --encodings utf-8 latin-1 foobar
 rm -f foo foo.c* foo.gc*
