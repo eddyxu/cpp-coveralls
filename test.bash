8bcdb3d36e07f1124437a3f1e1c44ea355036423
Steven Myint <git@stevenmyint.com>
2014-05-02 07:53:33 -0700

    Add back missing grep

--- test.bash
+++ test.bash
@@ -7,5 +7,6 @@ coveralls --help
 echo 'int main() {return 0;}' > foo.c
 gcc -coverage -o foo foo.c
 ./foo
-coveralls --verbose --encodings utf-8 latin-1 foobar
+coveralls --verbose --encodings utf-8 latin-1 foobar | \
+    grep 'coverage' | grep '1'
 rm -f foo foo.c* foo.gc*
