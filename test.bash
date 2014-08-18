a5b5417e201ca6fc1d5ecea00890dec4fc730da6
Remi Rampin <remirampin@gmail.com>
2014-08-18 19:05:51 -0400

    Updates test to use LCOV-style exclusions

--- test.bash
+++ test.bash
@@ -4,9 +4,26 @@
 
 coveralls --help
 
-echo 'int main() {return 0;}' > foo.c
+cat > foo.c <<'EOF'
+int main() {
+    int a = 1;
+    if(a == 2) {
+        a = 3;
+        /* LCOV_EXCL_START */
+        a = 4;
+        a = 5;
+        /* LCOV_EXCL_END */
+        a = 6;
+    }
+    if(a == 7) {
+        a = 8;
+        a = 9; /* LCOV_EXCL_LINE */
+    }
+    return 0;
+}
+EOF
 gcc -coverage -o foo foo.c
 ./foo
 coveralls --verbose --encodings utf-8 latin-1 foobar | \
-    grep 'coverage' | grep '1'
+    grep '\[1, 1, 1, 0, None, None, None, None, 0, None, 1, 0, None, None, 1, None\]'
 rm -f foo foo.c* foo.gc*
