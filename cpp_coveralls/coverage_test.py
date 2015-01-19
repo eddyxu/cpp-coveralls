e9dceb8829dc8bf5bbb05313a4be24a7e9485523
Lei Xu <lei@cloudera.com>
2015-01-18 17:17:34 -0800

    Be able to exclude files in include directories

--- cpp_coveralls/coverage_test.py
+++ cpp_coveralls/coverage_test.py
@@ -1,31 +1,48 @@
 #!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 #
 # Copyright 2014 (c) Lei Xu <eddyxu@gmail.com>
 
 import tempfile
+import unittest
 
 from . import coverage
 
+class CoverageTest(unittest.TestCase):
+    def test_exclude_pattern(self):
+        """Test using regular expression for exclusion rules."""
+        args = coverage.create_args(['-E', r'/abc.*\.txt'])
+        self.assertTrue(coverage.is_excluded_path(args, '/abcd.txt'))
+        self.assertFalse(coverage.is_excluded_path(args, '/cd.txt'))
 
-def test_exclude_pattern():
-    """Test using regular expression for exclusion rules."""
-    args = coverage.create_args(['-E', r'/abc.*\.txt'])
-    assert coverage.is_excluded_path(args, '/abcd.txt')
-    assert not coverage.is_excluded_path(args, '/cd.txt')
+    def test_mix_include_and_exclude_patten(self):
+        """Test mix include pattens with exludes."""
+        args = coverage.create_args(
+            ("--include src --exclude src/not-this-file.c --exclude-pattern " +
+            ".*useless.*").split())
+        self.assertTrue(coverage.is_excluded_path(args,
+                                                  "src/not-this-file.c"))
+        self.assertFalse(coverage.is_excluded_path(args,
+                                                   "src/but-this-file.c"))
+        self.assertTrue(coverage.is_excluded_path(args,
+                                                  "src/someuselessthing"))
 
 
-def test_try_encodings():
-    with tempfile.NamedTemporaryFile(mode='wb') as output_file:
-        output_file.write(b'\xe8')
-        output_file.flush()
+    def test_try_encodings(self):
+        with tempfile.NamedTemporaryFile(mode='wb') as output_file:
+            output_file.write(b'\xe8')
+            output_file.flush()
 
-        assert coverage.try_encodings(output_file.name,
-                                      ['utf-8', 'latin-1']) == 'latin-1'
+            assert coverage.try_encodings(output_file.name,
+                                          ['utf-8', 'latin-1']) == 'latin-1'
 
-        exception = None
-        try:
-            coverage.try_encodings(output_file.name,
-                                   ['utf-8'])
-        except ValueError as temporary:
-            exception = temporary
-        assert exception
+            exception = None
+            try:
+                coverage.try_encodings(output_file.name,
+                                       ['utf-8'])
+            except ValueError as temporary:
+                exception = temporary
+            assert exception
+
+if __name__ == '__main__':
+    unittest.main()
